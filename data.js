9c07d5d3bf02f5fae30a7dbdd428ec57da192a64
benlongcp <164564032+benlongcp@users.noreply.github.com>
2024-05-27 20:32:08 -0500

    day 1 of matchy classwork

--- data.js
+++ data.js
@@ -11,20 +11,39 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//var animal creates an empty object
+var animal = {};
+//dot notation to add a species key and value
+animal.species = "cat";
+//bracket notation to add a name key and value
+animal['name'] = "Fiddlesticks";
+//dot notation to add an empty array
+animal.noises = [];
+//logs
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+var noises = [];
+noises[0] = "meow";
+noises.push("purr");
+noises.unshift("yowl");
+noises[noises.length] = "hiss";
 
+console.log(noises.length);
+console.log(noises[length - 1]);
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+animal['noises'] = noises;
+animal['noises'].push("whine");
+console.log(animal);
 
 /* *******************************************************************
  * Step 4 - Review
@@ -35,6 +54,10 @@
  *
  * *******************************************************************
  */
+//accessing properties in objects can use dot notation or bracket notation
+
+//arrays only use bracket notation
+
 
 /* *******************************************************************
  * Step 5 - Take a Break!
@@ -48,13 +71,64 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+//crates an empty array animals
+var animals = [];
+//pushes our animal object to the animals array
+animals.push(animal);
+console.log(animals);
 
+//creates variable duck assigned to an object
+var duck = {
+  species: "duck",
+  name: "Jerome",
+  noises: ['quack', 'honk', 'sneeze', 'whoosh']
+}
 
+animals.push(duck);
+console.log (animals);
+
+var turtle = {
+  species: "turtle",
+  name: "Yurtle",
+  noises: ["grumble", "moan", 'squeal', "chew"]
+}
+
+animals.push(turtle);
+console.log(animals);
+
+var seagull = {
+  species: "seagull",
+  name: "Garbaggio",
+  noises: ["squawk", "screech", "vomit", "snap"]
+
+}
+
+animals.push(seagull);
+console.log(animals);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+ 
+//creates an empty array
+var friends = [];
 
+//creates a function that takes an array as its parameter
+ function getRandom(array){
+  //assign variable randIndex to the a random integer between 1 and 4
+  var randIndex = Math.floor(Math.random() * array.length);
+  //return the random index
+  return randIndex
+
+
+ } 
+ //add a random animal from animals to the friends array
+friends.push(animals[getRandom(animals)].name);
+console.log("friends: " + friends);
+
+//add the friends array as a key value in an animal
+animals[0]['friends'] = friends;
+console.log(animals[0]);
 
 
 /**
