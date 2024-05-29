711db0d447576a0f1f482ba2161cbc033ef67712
benlongcp <164564032+benlongcp@users.noreply.github.com>
2024-05-28 21:35:39 -0500

    day2 matchy classwork

--- functions.js
+++ functions.js
@@ -12,25 +12,73 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//implement function search that takes two parameters, an array and a string
+function search(array, string){
+    //loops through the input array animals
+    for (let i = 0; i < array.length; i++){
+        //check if name string is equal to the name property of the current iterated object in the array
+        if (string === array[i]['name']){
+            console.log("animals: " + array[i])
+            //return the animal object at that index
+            return array[i];
+        }
+            
+        }
+        return null;
+    }
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//creates a function that takes an array, a string, and an object as parameters
+function replace(array, string, object){
+    //loop through the animals array
+    for (let i = 0; i < array.length; i++){
+        //check if the string is equal to the name property of the current iterated object
+        if (string === array[i]['name']){
+            array[i] = object;
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(array, string){
+    //loops through the animals array
+    for (let i = 0; i < array.length; i++){
+        //check if the string is equal to the name property of the current iterated object
+        if(string === array[i]['name']){
+            array.splice(i, 1);
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+function add(array, object){
+    //checks if the name and species properties have lengths greater than 0
+    if (object.name.length > 0 && object.species.length > 0){
+        //create a cumulative check variable
+        let check = true;    
+        //loop through the array
+        for (let i = 0; i < array.length; i++){
+            //if the object name property is the same as the array name property, check becomes false
+            if (object.name === array[i].name){
+                check = false;
+            }
+        //only push the object to the array if check remains true.
+        if (check === true){
+            array.push(object);
+        }
+        }
 
+        }
+        }
 
 
 /**
